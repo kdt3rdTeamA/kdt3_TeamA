b05211d5dc4062edd4a94771ec957be7bab7fda7
오성인 <dhtjddls12@naver.com>
2022-10-12 16:16:51 +0900

    osi design

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-let ws = [""]
+let btn = $('.timeSection .list li');
+let btnSec = $('.list');
+let themeSec = $('.themeList');
+let themeli = $('.themeList li');
+
+btn.click(function() {
+    btnSec.toggleClass('dp-none');
+    themeSec.toggleClass('dp-none');
+});
+
+// if (btn.hasClass(back))
+
+
