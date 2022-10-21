828c9e0b3854cbdb2d4b7c9046ef288486b2e874
buhnopy <buhnopy@gmail.com>
2022-10-21 12:28:05 +0900

    roulette

---
+++ static/js/roulette_il.js
@@ -0,0 +1,68 @@
+const $c = document.querySelector("canvas");
+const ctx = $c.getContext(`2d`);
+
+
+const product = [
+  "초밥", '돈카츠', "라멘", "소바", "스키야키", "오니기리", '오코노미야키', "야키토리", "우동",
+];
+
+const colors = ["#ff8e7f", "#6ccad0", "#cbbeb5", "#ffe4e1 ", "#929292", "#ff8e7f", "#6ccad0", "#cbbeb5", "#ffe4e1", "#cbbeb5", "#929292"];
+
+const newMake = () => {
+    const [cw, ch] = [$c.width / 2, $c.height / 2];
+    const arc = Math.PI / (product.length / 2);
+  
+    for (let i = 0; i < product.length; i++) {
+      ctx.beginPath();
+      ctx.fillStyle = colors[i % (colors.length -1)];
+      ctx.moveTo(cw, ch);
+      ctx.arc(cw, ch, cw, arc * (i - 1), arc * i);
+      ctx.fill();
+      ctx.closePath();
+    }
+
+    ctx.fillStyle = "#fff";
+    ctx.font = "18px Pretendard";
+    ctx.textAlign = "center";
+
+    for (let i = 0; i < product.length; i++) {
+      const angle = (arc * i) + (arc / 2);
+
+      ctx.save()  ;
+
+      ctx.translate(
+        cw + Math.cos(angle) * (cw - 50),
+        ch + Math.sin(angle) * (ch - 50),
+      );
+
+      ctx.rotate(angle + Math.PI / 2);
+
+      product[i].split(" ").forEach((text, j) => {
+        ctx.fillText(text, 0, 30 * j);
+      });
+
+      ctx.restore();
+    }
+}
+
+const rotate = () => {
+  $c.style.transform = `initial`;
+  $c.style.transition = `initial`;
+  
+  setTimeout(() => {
+    
+    const ran = Math.floor(Math.random() * product.length);
+
+    const arc = 360 / product.length;
+    const rotate = (ran * arc) + 3600 + (arc * 3) - (arc/4);
+    
+    $c.style.transform = `rotate(-${rotate}deg)`;
+    $c.style.transition = `2s`;
+    
+    setTimeout(() => alert(`오늘의 야식은?! ${product[ran]} 어떠신가요?`), 2000);
+  }, 1);
+
+
+};
+
+newMake();
